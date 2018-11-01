5cd1a376b8689c5e3b488d24334a1da63cff21ed
iquality <155174391@qq.com>
2018-11-01 14:50:57 +0800

    remove 0.0.1 code

--- teamcat_web/doraemon/deploy_script/package.sh
+++
@@ -1,6 +0,0 @@
-cp logconfig.conf ../doraemon/logconfig.conf
-cp settings.py ../doraemon/settings.py
-cp web_uwsgi.ini ../web_uwsgi.ini
-cp websocket_uwsgi.ini ../websocket_uwsgi.ini
-rm -rf ../logs
-mkdir ../logs
--- teamcat_web/doraemon/deploy_script/package_test.sh
+++
@@ -1,6 +0,0 @@
-cp logconfig.conf ../doraemon/logconfig.conf
-cp test_settings.py ../doraemon/settings.py
-cp testweb_uwsgi.ini ../testweb_uwsgi.ini
-cp testwebsocket_uwsgi.ini ../testwebsocket_uwsgi.ini
-rm -rf ../logs
-mkdir ../logs
