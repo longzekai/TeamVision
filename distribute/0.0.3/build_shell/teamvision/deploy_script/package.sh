b46baffa536aa4674a4436dcc25fd8d2f584a72a
iquality <155174391@qq.com>
2019-01-23 15:06:25 +0800

    update 0.0.3 release

--- distribute/0.0.3/build_shell/teamvision/deploy_script/package.sh
+++
@@ -1,6 +0,0 @@
-cp logconfig.conf ../teamvision/logconfig.conf
-cp settings.py ../teamvision/settings.py
-cp web_uwsgi.ini ../web_uwsgi.ini
-cp websocket_uwsgi.ini ../websocket_uwsgi.ini
-rm -rf ../logs
-mkdir ../logs
